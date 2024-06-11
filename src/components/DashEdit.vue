626a44a95ff38a7c4948b0ce742af543327b09da
Volkov Alexander <volkov.525@gmail.com>
2024-06-11 13:06:36 +0300

    add remove item

--- src/components/DashEdit.vue
+++ src/components/DashEdit.vue
@@ -3,54 +3,72 @@ import { ref, reactive, computed } from 'vue'
 import { useStore } from '@/stores/store'
 import { GridItem, GridLayout } from 'vue-ts-responsive-grid-layout'
 import { templateRef, useElementSize } from '@vueuse/core'
+import { uid } from 'quasar'
 
 const store = useStore()
 const grid = templateRef('grid')
 
+const colNum = ref(12)
+const index = ref(1)
 const layout: any[] = reactive([
 	{
 		x: 0,
 		y: 0,
 		w: 3,
 		h: 3,
-		i: 'el',
+		i: 0,
 		set: false,
 		data: { chart: 0, table: 2 },
 	},
 ])
+
+const add = () => {
+	layout.push({
+		x: (layout.length * 3) % (colNum.value || 12),
+		y: layout.length + (colNum.value || 12), // puts it at the bottom
+		w: 3,
+		h: 3,
+		i: index.value,
+	})
+	index.value += 1
+}
+
+const remove = (e: number) => {
+	let temp = [...document.getElementsByClassName('vue-grid-item')]
+	temp.forEach((el) => el.classList.add('move'))
+	layout.splice(e, 1)
+}
 </script>
 
 <template lang="pug">
 q-btn.fab(round color="primary" icon="mdi-plus" @click="add" size="lg") 
 
-component.test(:is="GridLayout"
-	ref="grid"
+GridLayout(ref="grid"
 	:layout.sync="layout"
 	:col-num="12"
 	:row-height="30"
 	:is-draggable="true"
 	:is-resizable="true"
+  :is-bounded="true"
 	:is-mirrored="false"
 	:vertical-compact="true"
 	:margin="[10, 10]"
 	:show-close-button="false"
 	:use-css-transforms="true")
 
-	component(:is="GridItem"
-		v-for="( item, index ) in layout"
-			:enableEditMode="editMode"
-			:x="item.x"
-			:y="item.y"
-			:w="item.w"
-			:h="item.h"
-			:i="item.i"
-			:show-close-button="false"
-			:key="item.i")
-			q-card
-				q-card-section
-					q-btn(flat color="primary" label="Настроить" @click="setup($event, item, index)" size="sm") 
-				q-icon.close(name="mdi-close" @click="remove(index)" dense)
-				q-icon.resize(name="mdi-resize-bottom-right" @click="" dense size="16px") 
+	GridItem(v-for="( item, index ) in layout"
+		:x="item.x"
+		:y="item.y"
+		:w="item.w"
+		:h="item.h"
+		:i="item.i"
+		:show-close-button="false"
+		:key="item.i")
+		q-card
+			q-card-section
+				q-btn(flat color="primary" label="Настроить" @click="setup($event, item, index)" size="sm") 
+			q-icon.close(name="mdi-close" @click="remove(index)" dense)
+			q-icon.resize(name="mdi-resize-bottom-right" @click="" dense size="16px") 
 
 </template>
 
@@ -94,5 +112,8 @@ component.test(:is="GridLayout"
 :deep(.vue-draggable-dragging) {
 	z-index: 1000;
 	box-shadow: 3px 3px 17px rgba(0, 0, 0, 0.4);
+}
+.move {
+	transition: 0.2s ease all;
 }
 </style>
